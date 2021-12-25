8ba9ffbed1adeda693e36c50a2e6e80e744a1def
AnjaniBhagya <96667930+AnjaniBhagya@users.noreply.github.com>
2021-12-25 15:56:29 -0700

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+
+
+public class Factorial {
+	
+
+	public static void main(String[] args) {
+		// Declare your input as 5
+		int num=5;
+		int fact=1;
+		
+		// Declare an integer variable fact as 1
+		
+
+		for (int i=1; i<=num; i++) 
+		{
+			fact=fact*i;
+			
+		}
+		System.out.println(fact);
+
+		// Print factorial (fact)
+	
+
+	}
+
+}
