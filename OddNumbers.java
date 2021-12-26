8fa2f5b82940af09750c3a096b7245db3eb35c3c
AnjaniBhagya <96667930+AnjaniBhagya@users.noreply.github.com>
2021-12-25 17:12:26 -0700

    Add files via upload

---
+++ OddNumbers.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+import java.util.Iterator;
+
+public class OddNumbers {
+
+	public static void main(String[] args) {
+		int max =20;
+		System.out.println("Odd Numbers :");
+		
+		for (int i=1; i<=max;i++) 
+		{
+          if(i % 2 == 1)
+          {
+        	  System.out.println(i);
+          }
+          }
+
+			
+		}
+
+	}
+
+
